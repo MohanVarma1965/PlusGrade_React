7a1df81faed2f029db95804563bf2796e8df16b1
mohan varma <mohanvarma.k@gmail.com>
2023-10-28 11:31:14 -0600

    adding a custom hook and moving the logic from wrapper component

--- src/components/TaxCalcWrapperComponent/TaxCalcWrapperComponent.tsx
+++ src/components/TaxCalcWrapperComponent/TaxCalcWrapperComponent.tsx
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { AppDispatch, RootState } from "../../redux/store";
-import { CalculateTaxAmountsUtil } from "../../utils/CalculateTaxAmountsUtil/CalculateTaxAmountsUtil";
+import React from "react";
+import { useTaxCalculation } from "../../customHooks/useTaxCalculation";
 import { SERVER_ERROR_PREPEND_TEXT, PLEASE_TRY_AGAIN_TEXT } from "../../constants/taxConstants";
-import { resetTaxBrackets, fetchTaxBrackets } from "../../redux/slices/taxCalculator/taxCalculatorSlice";
 import Spinner from "../../utils/Spinner/Spinner";
 import TaxFormComponent from "../TaxFormComponent/TaxFormComponent";
 import Notification from "../../utils/Notifications/Notification";
@@ -11,50 +8,15 @@ import TaxDisplayComponent from "../TaxDisplayComponent/TaxDisplayComponent";
 import TaxChartComponent from "../TaxChartComponent/TaxChartComponent";
 import "./TaxCalculatorWrapper.css";
 
-// Main Tax Calculation Wrapper Component
 const TaxCalcWrapperComponent: React.FC = () => {
-  // Set default values for TaxFormComponent
-  const [income, setIncome] = useState<number>(125000);
-  const [year, setYear] = useState<number>(2022);
-
-  // Set Local state
-  const [taxAmounts, setTaxAmounts] = useState<number[]>([]);
-
-  // Grab below details from store
-  const { loading, taxBrackets, error } = useSelector((state: RootState) => ({
-    loading: state.taxCalculator.loading,
-    taxBrackets: state.taxCalculator.taxBrackets,
-    error: state.taxCalculator.error,
-  }));
-
-  // Dispatch
-  const dispatch = useDispatch<AppDispatch>();
-
-  // Handle calculation function for fetching tax brackets
-  const handleCalculate = () => {
-    dispatch(fetchTaxBrackets(year));
-  };
-
-  // Handle year changes and reset relevant state and Redux store data
-  const handleSetYearChange = (newYear: number) => {
-    setYear(newYear);
-    // Reset tax amounts when year changes
-    setTaxAmounts([]);
-    dispatch(resetTaxBrackets());
-  };
-
-  // useEffect to recalculate tax amounts when income or tax brackets change
-  useEffect(() => {
-    const newTaxAmounts = CalculateTaxAmountsUtil(income, taxBrackets);
-    setTaxAmounts(newTaxAmounts);
-  }, [income, taxBrackets]);
+  // Custom hook to encapsulate tax calculation logic and state management.
+  const { income, setIncome, year, loading, error, taxAmounts, handleCalculate, handleSetYearChange } =
+    useTaxCalculation(125000, 2022);
 
   return (
     <React.Fragment>
-      {/* This will attach the spinner if loading is set to true*/}
       {loading && <Spinner />}
       {error && <Notification arrMessages={[SERVER_ERROR_PREPEND_TEXT, error, PLEASE_TRY_AGAIN_TEXT]} />}
-      {/* Tax Form component for user inputs*/}
       <TaxFormComponent
         income={income}
         year={year}
@@ -66,11 +28,9 @@ const TaxCalcWrapperComponent: React.FC = () => {
       {/*Display tax details and chart only if there are tax amounts and no errors */}
       {taxAmounts.length > 0 && !error && (
         <div className="components-container">
-          {/* This will display the tax details and tax bracket table based on the tax amounts*/}
           <div className="tile">
             <TaxDisplayComponent taxAmounts={taxAmounts} income={income} />
           </div>
-          {/* This will display the tax tax chart*/}
           <div className="tile">
             <TaxChartComponent taxAmounts={taxAmounts} />
           </div>
---
+++ src/customHooks/useTaxCalculation.ts
@@ -0,0 +1,51 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { CalculateTaxAmountsUtil } from "../utils/CalculateTaxAmountsUtil/CalculateTaxAmountsUtil";
+import { resetTaxBrackets, fetchTaxBrackets } from "../redux/slices/taxCalculator/taxCalculatorSlice";
+import { AppDispatch, RootState } from "../redux/store";
+
+// Custom hook to encapsulate tax calculation logic and state management.
+export const useTaxCalculation = (initialIncome: number, initialYear: number) => {
+  const [income, setIncome] = useState<number>(initialIncome);
+  const [year, setYear] = useState<number>(initialYear);
+  const [taxAmounts, setTaxAmounts] = useState<number[]>([]);
+
+  // Fetch the current loading state, tax brackets data, and potential errors from the Redux store.
+  const { loading, taxBrackets, error } = useSelector((state: RootState) => ({
+    loading: state.taxCalculator.loading,
+    taxBrackets: state.taxCalculator.taxBrackets,
+    error: state.taxCalculator.error,
+  }));
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleCalculate = () => {
+    dispatch(fetchTaxBrackets(year));
+  };
+
+  // Function to handle changes in the year selection.
+  // Resets the tax amounts and tax bracket data upon change.
+  const handleSetYearChange = (newYear: number) => {
+    setYear(newYear);
+    setTaxAmounts([]);
+    dispatch(resetTaxBrackets());
+  };
+
+  // Recompute tax amounts whenever income or tax brackets change.
+  useEffect(() => {
+    const newTaxAmounts = CalculateTaxAmountsUtil(income, taxBrackets);
+    setTaxAmounts(newTaxAmounts);
+  }, [income, taxBrackets]);
+
+  // Return all states and handlers to be utilized by components using this custom hook.
+  return {
+    income,
+    setIncome,
+    year,
+    loading,
+    error,
+    taxAmounts,
+    handleCalculate,
+    handleSetYearChange,
+  };
+};
